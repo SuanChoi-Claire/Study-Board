4f8e490db9cf4d334fb92305bf6f8d7fdea762ed
SuanChoi-Claire <clairechoi.h@gmail.com>
2025-03-07 18:43:45 +0900

    first commit

---
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -0,0 +1,65 @@
+package com.mysite.sbb.question;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.RequiredArgsConstructor;
+//import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import jakarta.validation.Valid;
+import org.springframework.validation.BindingResult;
+import com.mysite.sbb.answer.AnswerForm;
+
+
+
+
+@RequestMapping("/question")
+@RequiredArgsConstructor
+@Controller
+public class QuestionController {
+	
+	private final QuestionService questionService;
+	
+	@GetMapping(value = "/detail/{id}")
+	public String detail(Model model, @PathVariable("id") Integer id, AnswerForm answerForm) {
+		
+		Question question = this.questionService.getQuestion(id);
+		model.addAttribute("question",question);
+		return "question_detail";
+	}
+	
+	//private final QuestionRepository questionRepository;
+	
+	
+	@GetMapping("/list")
+//	@ResponseBody
+	public String list(Model model, @RequestParam(value="page", defaultValue="0")int page) {
+		Page<Question> paging = this.questionService.getList(page);
+		model.addAttribute("paging", paging);
+		return "question_list";
+				
+	}
+	
+	@GetMapping("/create")
+	public String questionCreate(QuestionForm questionForm) {
+		return "question_form";
+	}
+	
+	@PostMapping("/create")
+	public String questionCreate
+	(@Valid QuestionForm questionForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "question_form";
+        }
+        this.questionService.create(questionForm.getSubject(), questionForm.getContent());
+		return "redirect:/question/list";
+	}
+
+}
