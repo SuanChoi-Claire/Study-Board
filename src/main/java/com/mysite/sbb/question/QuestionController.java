6b3d8c781c4394da21147424cb525e9e61da580a
SuanChoi-Claire <clairechoi.h@gmail.com>
2025-03-11 20:32:18 +0900

    글,댓글 수정 삭제 기능추가+ 댓글 살렸다

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -1,25 +1,27 @@
 package com.mysite.sbb.question;
 
-import java.util.List;
 import java.security.Principal;
-import com.mysite.sbb.user.SiteUser;
-import com.mysite.sbb.user.UserService;
 
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 //import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import jakarta.validation.Valid;
-import org.springframework.validation.BindingResult;
+import org.springframework.web.server.ResponseStatusException;
+
 import com.mysite.sbb.answer.AnswerForm;
-import org.springframework.security.access.prepost.PreAuthorize;
+import com.mysite.sbb.user.SiteUser;
+import com.mysite.sbb.user.UserService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 
 
 
@@ -70,4 +72,44 @@ public class QuestionController {
 		return "redirect:/question/list";
 	}
 
+	 @PreAuthorize("isAuthenticated()")
+	    @GetMapping("/modify/{id}")
+	    public String questionModify(QuestionForm questionForm, @PathVariable("id") Integer id, Principal principal) {
+	        Question question = this.questionService.getQuestion(id);
+	        if(!question.getAuthor().getUsername().equals(principal.getName())) {
+	            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+	        }
+	        questionForm.setSubject(question.getSubject());
+	        questionForm.setContent(question.getContent());
+	        return "question_form";
+	    }
+	
+	 @PreAuthorize("isAuthenticated()")
+	    @PostMapping("/modify/{id}")
+	    public String questionModify(@Valid QuestionForm questionForm, BindingResult bindingResult, 
+	            Principal principal, @PathVariable("id") Integer id) {
+	        if (bindingResult.hasErrors()) {
+	            return "question_form";
+	        }
+	        Question question = this.questionService.getQuestion(id);
+	        if (!question.getAuthor().getUsername().equals(principal.getName())) {
+	            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+	        }
+	        this.questionService.modify(question, questionForm.getSubject(), questionForm.getContent());
+	        return String.format("redirect:/question/detail/%s", id);
+	    }
+	 
+	
+	 @PreAuthorize("isAuthenticated()")
+	    @GetMapping("/delete/{id}")
+	    public String questionDelete(Principal principal, @PathVariable("id") Integer id) {
+	        Question question = this.questionService.getQuestion(id);
+	        if (!question.getAuthor().getUsername().equals(principal.getName())) {
+	            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "삭제권한이 없습니다.");
+	        }
+	        this.questionService.delete(question);
+	        return "redirect:/";
+	    }
+	 
+	
 }
